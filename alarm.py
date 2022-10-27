14d3390831aeb29a33623d75b6b50f69021cc76f
ljstaib <ljstaib@bu.edu>
2022-10-27 16:20:41 -0400

    GUI fixed, basic timer created

--- alarm.py
+++ alarm.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
 
 import sys
-from PyQt6.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QInputDialog
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QLabel, QInputDialog
 from PyQt6.QtCore import QTimer
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__(parent=None)
 
         self.setWindowTitle('Study Timer')
         self.setGeometry(100, 100, 400, 600)
-        self.getUI()
+        self._getUI()
     
-    def getUI(self):
+    def _getUI(self):
         # Variables
         self.count, self.start = 0, False
 
         # Create button to set the length of the timer
-        self.setTimeButton = QPushButton('Set time')
+        self.setTimeButton = QPushButton('Set time', self)
         self.setTimeButton.setGeometry(125, 100, 150, 50)
-        self.setTimeButton.clicked.connect(self.getTime)
+        self.setTimeButton.clicked.connect(self._getTime)
 
         # Create label to show time left
-        self.label = QLabel("//TIMER//")
+        self.label = QLabel("//TIMER//", self)
         self.label.setGeometry(100, 200, 200, 50)
         # self.label.setAlignment(QtCore.Qt.AlignCenter)
 
         # Create start button
-        self.startButton = QPushButton("Start")
+        self.startButton = QPushButton("Start", self)
         self.startButton.setGeometry(125, 350, 150, 40)
-        self.startButton.clicked.connect(self.startTimer)
+        self.startButton.clicked.connect(self._startTimer)
 
         # Create pause button
-        self.pauseButton = QPushButton("Pause")
+        self.pauseButton = QPushButton("Pause", self)
         self.pauseButton.setGeometry(125, 400, 150, 40)
-        self.pauseButton.clicked.connect(self.pauseTimer)
+        self.pauseButton.clicked.connect(self._pauseTimer)
 
         # Create reset button
-        self.resetButton = QPushButton("Reset")
+        self.resetButton = QPushButton("Reset", self)
         self.resetButton.setGeometry(125, 450, 150, 40)
-        self.resetButton.clicked.connect(self.resetTimer)
+        self.resetButton.clicked.connect(self._resetTimer)
 
         # Create timer
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.showTimer)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self._showTimer)
         self.timer.start(100)
 
-    def getTime(self):
+    def _getTime(self):
         self.start = False
 
         seconds_in, isDone = QInputDialog.getInt(self, 'Seconds', 'Enter Seconds:')
@@ -57,20 +57,20 @@ class Window(QWidget):
             self.label.setText(str(seconds_in))
 
 
-    def startTimer(self):
+    def _startTimer(self):
         self.start = True
 
         if self.count == 0:
             self.start = False
     
-    def pauseTimer(self):
+    def _pauseTimer(self):
         self.start = False
 
-    def resetTimer(self):
+    def _resetTimer(self):
         self.count, self.start = 0, False
         self.label.setText("//TIMER//")
 
-    def showTimer(self):
+    def _showTimer(self):
         if self.start:
             self.count -= 1
 
@@ -83,6 +83,6 @@ class Window(QWidget):
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = Window()
-    window.show()
+    win = Window()
+    win.show()
     sys.exit(app.exec())
